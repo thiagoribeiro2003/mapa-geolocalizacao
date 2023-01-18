0b90fc92e8368710ad42b7de9ea023391a044eba
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2023-01-18 09:37:11 -0300

    Usando StatusBar do React-native

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar />
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    </>
   );
 }
 
