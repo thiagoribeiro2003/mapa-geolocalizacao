acdad61f1ecfcddabecec65d8756358bee3b6be5
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2023-01-18 09:54:10 -0300

    Testando região utilizando valores de latitude e longitude

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
 import { StyleSheet, Text, View, StatusBar } from "react-native";
+import MapView from "react-native-maps";
 
 export default function App() {
+  const regiaoInicial = {
+    latitude: 37.78825,
+    longitude: -122.4324,
+    // latitudeDelta: 0.09,
+    // longitudeDelta: 0.0421,
+    latitudeDelta: 2,
+    longitudeDelta: 2,
+  };
+
   return (
     <>
       <StatusBar />
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={estilos.container}>
+        <MapView style={estilos.mapa} initialRegion={regiaoInicial} />
       </View>
     </>
   );
 }
 
-const styles = StyleSheet.create({
+const estilos = StyleSheet.create({
+  mapa: {
+    width: "100%",
+    height: "100%",
+  },
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
