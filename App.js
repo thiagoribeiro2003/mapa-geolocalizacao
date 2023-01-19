a4ac3ff8fd4a77a63a1bc7b7539a983d585afe31
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2023-01-19 09:38:54 -0300

    Fazendo marcador interativo com o clique

--- App.js
+++ App.js
@@ -11,27 +11,32 @@ import MapView, { Marker } from "react-native-maps";
 
 export default function App() {
   const regiaoInicial = {
-    latitude: -10,
-    longitude: -55,
+    // Estado de SP
+    latitude: -23.533773,
+    longitude: -46.65529,
     // latitudeDelta: 0.09,
     // longitudeDelta: 0.0421,
     latitudeDelta: 10,
     longitudeDelta: 10,
   };
 
-  const localizacao = {
-    latitude: -33.861886,
-    longitude: -63.987,
-    latitudeDelta: 10,
-    longitudeDelta: 10,
-  };
-
+  /* Usando state para controlar a localização*/
   const [localizacaoClicada, setLocalizacaoClicada] = useState({
     latitude: -33.861886,
     longitude: -63.987,
     latitudeDelta: 10,
     longitudeDelta: 10,
   });
+
+  const marcarLocal = (event) => {
+    setLocalizacaoClicada({
+      ...localizacaoClicada, // o spred (...) junta os dados
+      latitude: event.nativeEvent.coordinate.latitude,
+      longitude: event.nativeEvent.coordinate.longitude,
+    });
+
+    console.log(localizacaoClicada);
+  };
 
   return (
     <>
@@ -41,22 +46,27 @@ export default function App() {
           style={estilos.mapa}
           initialRegion={regiaoInicial}
           liteMode={false} // somente Android
-          mapType="satellite" // satellite, hybrid, standard
+          mapType="standard" // satellite, hybrid, standard
           userInterfaceStyle="dark" // somente IOS
           // maxZoomLevel={15}
           // minZoomLevel={1}
-          onPress={localizacaoClicada}
+
+          onPress={(e) =>
+            setLocalizacaoClicada({
+              ...localizacaoClicada, // o spred (...) junta os dados
+              latitude: e.nativeEvent.coordinate.latitude,
+              longitude: e.nativeEvent.coordinate.longitude,
+            })
+          }
         >
           <Marker
+            coordinate={localizacaoClicada}
             draggable
-            coordinate={localizacao}
+            //coordinate={localizacao}
             title="Aqui!!!"
-            onPress={(event) => {
-              console.log(event.NativeEvent);
-            }}
-          >
-            {/* <Image source={require("./assets/iconeMarcador.png")} /> */}
-          </Marker>
+            onPress={(e) => console.log(e.nativeEvent)}
+          />
+          {/* <Image source={require("./assets/iconeMarcador.png")} /> */}
         </MapView>
       </View>
     </>
